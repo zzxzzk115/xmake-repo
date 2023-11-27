c84346e2da2fb718449ba7d87341633a99047187
Lazy_V <954294627@qq.com>
2023-11-27 18:19:58 +0000

    update GENie for RPI CM4.

--- packages/g/genie/xmake.lua
+++ packages/g/genie/xmake.lua
@@ -1,15 +1,15 @@
 package("genie")
 
     set_kind("binary")
-    set_homepage("https://github.com/bkaradzic/GENie")
-    set_description("GENie - Project generator tool")
+    set_homepage("https://github.com/zzxzzk115/GENie")
+    set_description("GENie - Project generator tool. Removed -m64 flag for Raspberry Pi and other embed linux platforms.")
 
     if is_host("windows") then
         add_urls("https://raw.githubusercontent.com/bkaradzic/bx/aa5090bbd8c39e84d483f4850a7d9ca7ff9241ed/tools/bin/windows/genie.exe")
         add_versions("1160.0", "a1226a0098f6ff4895a423df17737af90cc0af9effce7cc81fe904c9b2e02e8f")
     else
-        add_urls("https://github.com/bkaradzic/GENie.git")
-        add_versions("1160.0", "0f5133b66d103efbcdf7c0d23f6cb6b353282e38")
+        add_urls("https://github.com/zzxzzk115/GENie.git")
+        add_versions("1160.0", "db6e8d595a0ee1e5f381e7afa22bd8e97df3e2e7")
     end
 
     on_install("@windows", function (package)
