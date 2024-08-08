6b0ca1b02339716c3a616eb1bfd2d4c3e465d9ca
Lazy_V <954294627@qq.com>
2024-08-08 18:36:21 +0100

    add configs.

--- packages/v/vgfw/xmake.lua
+++ packages/v/vgfw/xmake.lua
@@ -7,6 +7,8 @@ package("vgfw")
              "https://github.com/zzxzzk115/vgfw.git")
 	
 	add_versions("v1.1.1", "51dfda6e280523ea50c59a2f7ff5a325f196b157d598709a6f145071c91b6160")
+	
+	add_configs("examples", {description = "Build examples", default = false, type = "boolean"})
 
     add_deps("fg", "glad", "glfw", "glm", "spdlog", "stb", "tinyobjloader", "tinygltf")
 	add_deps("imgui v1.90.8-docking", {configs = {glfw = true, opengl3 = true, wchar32 = true}})
@@ -16,5 +18,8 @@ package("vgfw")
     end
 
     on_install("windows", "linux", function (package)
-        import("package.tools.xmake").install(package)
+		local configs = {
+			examples = package:config("examples")
+		}
+        import("package.tools.xmake").install(package, configs)
     end)
