c44de11232afd07ff25ed9205cada7bda696f8ee
Kexuan Zhang <sc23kz@leeds.ac.uk>
2025-08-04 17:23:08 +0100

    libvultra: update version to 2025.08.042

--- packages/l/libvultra/xmake.lua
+++ packages/l/libvultra/xmake.lua
@@ -5,7 +5,7 @@ package("libvultra")
 
     add_urls("https://github.com/zzxzzk115/libvultra.git")
 
-    add_versions("2025.08.041", "658d180d4511ef28295fc1e94649c26a0a9fa265")
+    add_versions("2025.08.042", "20e98f588b9ec0119ffe0757b158facc71237567")
     add_versions("2025.08.021", "eab2a7de77d621fa05df2830293575b1b87721e9")
 
     add_configs("tracy", {description = "Enable Tracy profiler support", default = true, type = "boolean"})
