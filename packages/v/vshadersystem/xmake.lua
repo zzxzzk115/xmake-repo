61e643ab0c3e997840e87fe1fe0679b504338546
Lazy_V <954294627@qq.com>
2026-02-27 22:50:44 +0000

    bump vshadersystem to 0.3.0

--- packages/v/vshadersystem/xmake.lua
+++ packages/v/vshadersystem/xmake.lua
@@ -6,6 +6,7 @@ package("vshadersystem")
     add_urls("https://github.com/zzxzzk115/vshadersystem/archive/refs/tags/$(version).tar.gz",
              "https://github.com/zzxzzk115/vshadersystem.git")
 
+    add_versions("v0.3.0", "b40115d6c334723c696ffc08d774a9a181af85b2892f0f76072679a6822e2658")
     add_versions("v0.2.0", "79a8894acfe8d3f67b604c51691a7720fb2fbb3e61c5ab406bd754e0e40c6019")
 
     add_deps("spirv-cross vulkan-sdk-1.4.309", {configs = { shared = true, debug = false }, system = false})
