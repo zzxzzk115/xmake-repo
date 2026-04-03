68c881c74874929e3f6003a2fded73bccacd94b8
Lazy_V <954294627@qq.com>
2026-04-03 21:57:46 +0100

    bump vshadersystem to 0.7.2 (prebuilt, WASM)

--- packages/v/vshadersystem/xmake.lua
+++ packages/v/vshadersystem/xmake.lua
@@ -7,45 +7,50 @@ package("vshadersystem")
     add_urls("https://github.com/zzxzzk115/vshadersystem.git", {alias = "git"})
 
     add_versions("source:v0.6.2", "73a381c343f856030574cc787f7e30d4d6e020db26cef40413ba7f6fd7170560")
-    add_versions("source:v0.7.1", "d501a2051679b3fad158e9560704f2ee49541d55e0d0681ee3613eb18e2b57b8")
-    add_versions("git:v0.7.1", "v0.7.1")
+    add_versions("source:v0.7.2", "a6e268d2cbc770e6ed8ffc8c554a9898db54ff7e2b045ee0af595286bce925fb")
+    add_versions("git:v0.7.2", "v0.7.2")
 
     on_load(function (package)
-        if package:version():ge("0.7.1") then
+        if package:version():ge("0.7.2") then
             local prebuilt = {
-                ["android-arm64-v8a"] = "885d57e6d5251b0a2b979f153e5aebdda28f0f920068c19775c664e0f2099974",
-                ["android-armeabi-v7a"] = "679891b3b6ce811a407bbf94f70a78cb482c8b05e48c530851d5e427c1c7cd59",
-                ["android-x86_64"] = "d73afcf02a0d0c167ad32c4d6a4f9161509c85c3d8bbe8b9aac9710d74f5f137",
-                ["linux-arm64"] = "e106b809d404fc3a60658a96c24871b64a676680867711339e364bd2585af79e",
-                ["linux-x86"] = "afa8ab72f2122bf5ab1775189be1dcb23a4fb7c3d9eb590995a22fe4b80ce9d3",
-                ["linux-i386"] = "afa8ab72f2122bf5ab1775189be1dcb23a4fb7c3d9eb590995a22fe4b80ce9d3",
-                ["linux-x64"] = "03aa22242670d2addc3c73d40a9ebb0db81d98f40436fdbaf81bd52c25203502",
-                ["linux-x86_64"] = "03aa22242670d2addc3c73d40a9ebb0db81d98f40436fdbaf81bd52c25203502",
-                ["macosx-arm64"] = "193c9662b8bdc433e8e6a496ec9bde78d62c4ac2652b2a3470629074656d426d",
-                ["windows-arm64"] = "103d7643189f8b389cfe749804a127765e25efe25d89db6d06179f2b08946415",
-                ["windows-x64"] = "e0cb0c7be0c9deea8398f290ffe8225ed11753c14cfba818a909711f11bf9b97",
-                ["windows-x86_64"] = "e0cb0c7be0c9deea8398f290ffe8225ed11753c14cfba818a909711f11bf9b97",
-                ["mingw-x64"] = "e0cb0c7be0c9deea8398f290ffe8225ed11753c14cfba818a909711f11bf9b97",
-                ["mingw-x86_64"] = "e0cb0c7be0c9deea8398f290ffe8225ed11753c14cfba818a909711f11bf9b97"
+                ["android-arm64-v8a"] = "7315bd9e0ef9e7273c0aab5eded48643b73e2b25988e9f64329ba8cebc0f5dfa",
+                ["android-armeabi-v7a"] = "f26fc0d1c44d02eee48c3d8dd72fbc7391f4908959722597929a399fd8bebbaa",
+                ["android-x86_64"] = "88baf88b1d7e355fecca91024256d5c7550947b97431b784f9326fa597d9923a",
+                ["linux-arm64"] = "8aa78814126817c050be017417a65f14e44a0862548fb8400dc5a4fd4b799e35",
+                ["linux-x86"] = "cbb311058c66ea9d2814956f76fe2de8360652a327ac36ea14b7a645188e49ef",
+                ["linux-i386"] = "cbb311058c66ea9d2814956f76fe2de8360652a327ac36ea14b7a645188e49ef",
+                ["linux-x64"] = "c999c454b9c9bca8e2b635a9695638d686a7f2bb6313c4f477876236fd9fbdb1",
+                ["linux-x86_64"] = "c999c454b9c9bca8e2b635a9695638d686a7f2bb6313c4f477876236fd9fbdb1",
+                ["macosx-arm64"] = "588aa4912dab2c4120a1fba5e32e02f748c0155a7097beed4589e614c12f95be",
+                ["wasm-wasm32"] = "62ab22f3d811c11287222593fa14e23b90517a324b8762a52c4b33e38e1d38c6",
+                ["windows-arm64"] = "e700c0f62605811e85ac9936b397376d0220deaa89fb640c18ae4ce6333e980f",
+                ["windows-x64"] = "dc350f9755cdaaba1da319a414105927d152b3ef3e9417e9e8b2af2b8fd26d0c",
+                ["windows-x86_64"] = "dc350f9755cdaaba1da319a414105927d152b3ef3e9417e9e8b2af2b8fd26d0c",
+                ["mingw-x64"] = "dc350f9755cdaaba1da319a414105927d152b3ef3e9417e9e8b2af2b8fd26d0c",
+                ["mingw-x86_64"] = "dc350f9755cdaaba1da319a414105927d152b3ef3e9417e9e8b2af2b8fd26d0c"
             }
             local assets = {
                 ["linux-x64"] = "linux-x86_64",
                 ["windows-x64"] = "windows-x64",
                 ["mingw-x64"] = "windows-x64",
                 ["mingw-x86_64"] = "windows-x64",
-                ["windows-x86_64"] = "windows-x64"
+                ["windows-x86_64"] = "windows-x64",
+                ["wasm-wasm32"] = "wasm-wasm32"
             }
             local key = package:plat() .. "-" .. package:arch()
             local sha = prebuilt[key]
             assert(sha, "package(vshadersystem): unsupported prebuilt target: " .. key)
             local asset = assets[key] or key
             package:set("urls", "https://github.com/zzxzzk115/vshadersystem/releases/download/$(version)/vshadersystem-prebuilt-$(version)-" .. asset .. ".zip")
-            package:add("versions", "v0.7.1", sha)
+            package:add("versions", "v0.7.2", sha)
             package:add("links", "vshadersystem", "spirv-tools", "tint")
             package:add("deps", "spirv-cross vulkan-sdk-1.4.335", {configs = {shared = true, debug = false}, system = false, public = true})
             package:add("deps", "glslang 1.4.335+0", {configs = {debug = false}, system = false, public = true})
             package:add("deps", "xxhash", {public = true})
             package:add("links", "xxhash")
+            if package:is_cross() then
+                package:add("deps", "vshadersystem~host", {kind = "binary", host = true, private = true})
+            end
         else
             package:add("deps", "spirv-cross vulkan-sdk-1.4.309", {configs = { shared = true, debug = false }, system = false, public = true})
             package:add("deps", "glslang 1.4.309+0", {configs = { debug = false }, system = false, public = true})
@@ -55,7 +60,7 @@ package("vshadersystem")
     end)
 
     on_install(function (package)
-        if package:version():ge("0.7.1") then
+        if package:version():ge("0.7.2") then
             local version = tostring(package:version())
             local rootdir = os.dirs("vshadersystem-prebuilt-" .. version .. "-*")[1]
             if not rootdir then
@@ -72,50 +77,53 @@ package("vshadersystem")
     end)
 
     on_test(function (package)
-        if package:version():ge("0.7.1") and not package:is_plat("android") then
+        if package:version():ge("0.7.2") and not package:is_cross() then
             os.vrun("vshaderc --help")
         end
+        if package:is_cross() or package:is_binary() then
+            return
+        end
         assert(package:check_cxxsnippets({test = [[
-            #include <vshadersystem/system.hpp>
-            #include <iostream>
+                #include <vshadersystem/system.hpp>
+                #include <iostream>
 
-            using namespace vshadersystem;
+                using namespace vshadersystem;
 
-            int main()
-            {
-                const char* source = R"(
+                int main()
+                {
+                    const char* source = R"(
 
-            #version 460
+                #version 460
 
-            #pragma vultra material
+                #pragma vultra material
 
-            #pragma vultra param baseColor default(1,1,1,1)
+                #pragma vultra param baseColor default(1,1,1,1)
 
-            layout(location = 0) out vec4 outColor;
+                layout(location = 0) out vec4 outColor;
 
-            void main()
-            {
-                outColor = vec4(1,0,0,1);
-            }
+                void main()
+                {
+                    outColor = vec4(1,0,0,1);
+                }
 
-            )";
+                )";
 
-                BuildRequest req;
+                    BuildRequest req;
 
-                req.source.virtualPath = "test.frag.vshader";
-                req.source.sourceText  = source;
+                    req.source.virtualPath = "test.frag.vshader";
+                    req.source.sourceText  = source;
 
-                req.options.stage = ShaderStage::eFrag;
+                    req.options.stage = ShaderStage::eFrag;
 
-                auto r = build_single_shader(req);
+                    auto r = build_single_shader(req);
 
-                if (!r.isOk())
-                    return 1;
+                    if (!r.isOk())
+                        return 1;
 
-                if (r.value().binary.spirv.empty())
-                    return 1;
+                    if (r.value().binary.spirv.empty())
+                        return 1;
 
-                return 0;
-            }
+                    return 0;
+                }
         ]]}, {configs = {languages = "c++23"}}))
     end)
