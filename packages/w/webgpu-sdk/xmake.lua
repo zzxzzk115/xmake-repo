b6513b3ada556f3ffe780b6d7660fcae6d6f8c30
Lazy_V <954294627@qq.com>
2026-04-04 04:24:21 +0100

    fix: fix rpath settings.

--- packages/w/webgpu-sdk/xmake.lua
+++ packages/w/webgpu-sdk/xmake.lua
@@ -24,7 +24,7 @@ package("webgpu-sdk")
         if not package:is_plat("wasm") then
             package:add("links", "wgpu_native")
             package:add("deps", "glfw", {public = true})
-            package:add("rpathdirs", path.join(package:installdir(), "lib"), {public = true})
+            package:add("ldflags", "-Wl,-rpath," .. package:installdir("lib"))
         else
             package:add("ldflags", "-sUSE_WEBGPU", "-sUSE_GLFW=3", {force = true, public = true})
         end
