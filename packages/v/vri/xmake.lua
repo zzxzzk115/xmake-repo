c2afa9ad1f24125a2552e21b5f112211a4055279
Lazy_V <954294627@qq.com>
2026-06-30 00:35:17 +0100

    vri: update v0.1.1 sha256 (re-tagged release)

--- packages/v/vri/xmake.lua
+++ packages/v/vri/xmake.lua
@@ -6,7 +6,7 @@ package("vri")
     add_urls("https://github.com/zzxzzk115/VRI/archive/refs/tags/$(version).tar.gz",
              "https://github.com/zzxzzk115/VRI.git")
 
-    add_versions("v0.1.1", "449a2a2fcbb95e5846a366b3c4dd3b9a0df26222739a36b697e45b25c8d48b4f")
+    add_versions("v0.1.1", "b91baa762dfdbe9273f348fa023a835f05776a73cb4c75c5c9dd7d511bbe3063")
     add_versions("v0.1.0", "6aba9d064743ae50840fc04b13474aaad568e196330ef0f11d27ad335fb60fec")
 
     -- Backend selection mirrors VRI's own options. Vulkan is the reference backend (default on);
